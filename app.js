b2bf281f34e83b23ed036dc5cd82c18529f5e3d3
xinyuan1 <zhiguow12@gmail.com>
2018-04-04 13:18:45 -0500

    finaly

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var test = require('./routes/test');
+var zhiguo = require('./routes/zhiguo');
+var zhixin = require('./routes/zhixin');
 
 var app = express();
 
@@ -26,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/test', test);
+app.use('/zhiguo', zhiguo);
+app.use('/zhixin', zhixin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
